554dede54f248e5d88ac75be19404e97fa135a1c
MikeLeeJons <meerzulee@gmail.com>
2018-12-09 13:47:01 +0500

    Add POST tests for POST /user/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -270,3 +270,41 @@ describe('POST /users', () => {
   })
 
 })
+
+describe('POST /users/login', () => {
+  it('should login and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.header['x-auth']).toExist
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toMatchObject({
+            access: 'auth',
+            token: res.headers['x-auth']
+          })
+          done()
+        })
+      })
+  })
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: 'sdlfknsdlfnsdlf'
+      })
+      .expect(400)
+      .end(done)
+  })
+})
